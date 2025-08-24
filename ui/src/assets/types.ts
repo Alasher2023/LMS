32677ee4f1f1841087ae324151a84d4297ca074f
Alasher2023 <ly.development2023@gmail.com>
2025-08-24 22:57:37 +0900

    修改type

--- ui/src/assets/types.ts
+++ ui/src/assets/types.ts
@@ -5,12 +5,16 @@ export type Select = {
 
 export type Paper = {
   id?: number
-  author: string
-  type: string
-  status: string
-  subject: string
-  grade: string
-  title: string
-  path: string
+  author?: string
+  type?: string
+  status?: string
+  subject?: string
+  grade?: string
+  title?: string
+  path?: string
   memo?: string
+  review_stage?: number
+  next_review_date?: string // Using string because JSON transport will serialize datetimes
+  last_reviewed_at?: string
+  due_date?: string
 }
